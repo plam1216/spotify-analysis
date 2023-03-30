05acc26806e3c188aed7f2b99e25c1abb933dbfd
Peter Lam <plam1216@gmail.com>
2023-03-30 16:10:01 -0400

    update playlists routes

--- backend/src/controllers/playlists.ts
+++ backend/src/controllers/playlists.ts
@@ -50,41 +50,29 @@ playlistsRouter.get('/playlists', async (req: Request, res: Response) => {
             SELECT * FROM playlists
         `
 
-        const playlistsNameResponse = await db.query(getPlaylists)
-        const playlistsNameData = playlistsNameResponse.rows
+        // array of playlists
+        const { rows } = await db.query(getPlaylists)
 
-        res.status(201).json(playlistsNameData)
+        res.status(201).json(rows)
     } catch (err) {
         console.log('Something went wrong!', err);
     }
 })
 
-// GET Playlist Songs
-playlistsRouter.get('/playlists/:id', async (req: Request, res: Response) => {
-    const playlistId = req.params.id
-
+// DELETE a Playlist
+playlistsRouter.delete('/playlists/:id', async (req: Request, res: Response) => {
     try {
-        // get playlist
-        const getPlaylist = `
-            SELECT name FROM playlists
-            WHERE id=${playlistId}
-        `
-        // get songs from that playlist
-        const getPlaylistSongs = `
-            SELECT * FROM songs
-            WHERE playlist_id=${playlistId}
+        const deletePlaylist = `
+            DELETE FROM playlists
+            WHERE id=$1
+            RETURNING *
         `
 
-        const playlistResponse = await db.query(getPlaylist)
-        const playlistData = playlistResponse.rows
+        const { rows } = await db.query(deletePlaylist, [req.params.id])
 
-        const playlistSongsResponse = await db.query(getPlaylistSongs)
-        const playlistSongsData = playlistSongsResponse.rows
-        
-
-        res.status(201).json({playlistData, playlistSongsData})
+        res.status(201).json(rows)
     } catch (error) {
-        res.status(401).json(error)
+        res.status(400).json(error)
     }
 })
 
@@ -99,95 +87,51 @@ playlistsRouter.post('/playlists', async (req: Request, res: Response) => {
         // search for playlist
         const playlistData = await spotifyApi.searchPlaylists(req.body.playlistName)
 
-        // get playlist name
-        const playlistName = [playlistData.body.playlists.items[0].name]
+        // get playlist name & image
+        const playlistName = playlistData.body.playlists.items[0].name
+        const playlistImage = playlistData.body.playlists.items[0].images[0].url
+
+        const playlistParams = [playlistName, playlistImage]
 
         // insert playlistName into 'playlists' table
         // ON CONFLICT: if name exists, do nothing (ie. dont insert)
         const insertPlaylistIntoPlaylistsTable = `
-            INSERT INTO playlists (name) 
-            VALUES ($1)
+            INSERT INTO playlists (name, image) 
+            VALUES ($1, $2)
             ON CONFLICT (name) DO NOTHING
+            RETURNING *
         `
 
-        await db.query(insertPlaylistIntoPlaylistsTable, playlistName)
+        // retrieve the ID of the newly inserted playlist
+        // if it reaches ON CONFLICT, returns empty array []
+        const { rows } = await db.query(insertPlaylistIntoPlaylistsTable, playlistParams)
+        const playlistId = rows[0].id
 
-        res.status(201).json(playlistName)
+        res.status(201).json({
+            id: playlistId,
+            playlistName: playlistName,
+            playlistImage: playlistImage
+        })
     } catch (error) {
         res.status(400).json(error)
     }
 })
 
-// Add Songs to Playlist
-playlistsRouter.post('/playlists/:id', async (req: Request, res: Response) => {
-    const authHeader = req.headers.authorization
-    const accessToken = authHeader?.split(' ')[1]
-
-    spotifyApi.setAccessToken(accessToken)
-
+// GET ONE Playlist
+playlistsRouter.get('/playlists/:id', async (req: Request, res: Response) => {
     try {
-        // playlist foreign key
-        const playlistFKId = req.params.id
-
-        // search for playlist
-        const playlistData = await spotifyApi.searchPlaylists(req.body.playlistName)
-
-        // get playlist URI
-        const playlistDataUri = playlistData.body.playlists.items[0].uri.split(':')[2]
-
-        // get playlist data
-        const playlistTracksResponse = await spotifyApi.getPlaylist(playlistDataUri)
-        const tracksData = playlistTracksResponse.body.tracks.items
-
-        // iterate through tracks
-        for (let i = 0; i < tracksData.length; i++) {
-            const audioFeaturesData = await spotifyApi.getAudioFeaturesForTrack(tracksData[i].track.id)
-
-            // console.log(audioFeaturesData)
-            // store values needed for table into array for parameterized query
-            const audioFeatures = [
-                tracksData[i].track.name,
-                audioFeaturesData.body.danceability,
-                audioFeaturesData.body.duration_ms,
-                audioFeaturesData.body.energy,
-                audioFeaturesData.body.key,
-                audioFeaturesData.body.loudness,
-                audioFeaturesData.body.tempo,
-                audioFeaturesData.body.id,
-                playlistFKId,
-            ]
-
-            // SQL query: insert song data to 'songs' table
-            const insertSongsIntoSongsTable = `
-                INSERT INTO songs (name, danceability, duration_ms, energy, key, loudness, tempo, spotifyId, playlist_id) 
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-            `
-
-            // Parameterized Query
-            await db.query(insertSongsIntoSongsTable, audioFeatures)
-        }
-
-        res.status(201).json(tracksData)
-    } catch (error) {
-        res.status(201).json(error)
-    }
-})
-
-// Delete songs from Playlist
-playlistsRouter.delete('/playlists/:id', async (req: Request, res: Response) => {
-    try {
-        const playlistId = req.params.id
-        const deleteSongsFromNMF = `
-            DELETE FROM songs
-            WHERE playlist_id=$1
+        const getPlaylist = `
+            SELECT * FROM playlists
+            WHERE id=$1
         `
-        await db.query(deleteSongsFromNMF, [playlistId])
 
-        res.status(201).json(req.body.playlistId)
+        const { rows } = await db.query(getPlaylist, [req.params.id])
+        const playlistData = rows[0]
+
+        res.status(201).json({ playlistData })
     } catch (error) {
-        res.status(400).json(error)
+        res.status(401).json(error)
     }
-
 })
 
 export default playlistsRouter
