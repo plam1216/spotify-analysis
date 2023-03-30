e3218c95c0df1774d26aa1e22a2c07bbcdac1ff0
Peter Lam <plam1216@gmail.com>
2023-03-30 16:11:52 -0400

    install react-router-dom

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -7,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   // <React.StrictMode>
+  <Router>
     <App />
+  </Router>
   // </React.StrictMode>
 );
