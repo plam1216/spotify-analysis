b72e618a05d192fc42690c2a7f3f16902ff7d3aa
Peter Lam <plam1216@gmail.com>
2023-03-31 00:53:22 -0400

    get accessToken and created topplaylist routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css';
+import Home from './pages/Home/Home';
+import PlaylistAnalysis from './pages/PlaylistAnalysis/PlaylistAnalysis';
+import TopPlaylists from './pages/TopPlaylists/TopPlaylists';
 
 function App() {
+  const [accessToken, setAccessToken] = useState('')
+
+  const getToken = async () => {
+    const response = await fetch('http://localhost:4000/api/token', {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    })
+
+    const data = await response.json()
+    setAccessToken(data.accessToken)
+  }
+
+  useEffect(() => {
+    getToken()
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/topplaylists' element={<TopPlaylists token={accessToken} />} />
+        <Route path='/topplaylists/:id' element={<PlaylistAnalysis token={accessToken} />} />
+      </Routes>
     </div>
   );
 }
