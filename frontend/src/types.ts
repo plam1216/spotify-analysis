ddc56e7645dd01f73d00c00517c3597aa7f8346a
Peter Lam <plam1216@gmail.com>
2023-04-02 11:57:05 -0400

    add artist and mode to Song interface

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,17 +1,19 @@
 export interface Song {
     id: number,
     name: string,
+    artist: string,
     danceability: number,
     duration_ms: number,
     energy: number,
     key: number,
     loudness: number,
+    mode: number,
     tempo: number,
     spotify_id: string,
     playlist_id: number,
 }
 
-export interface Playlists {
+export interface Playlist {
     id: number,
     name: string,
     image: string,
